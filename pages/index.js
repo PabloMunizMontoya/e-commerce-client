71d7b1df920604c20ca31de40d9be19460c0a514
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-07-26 23:27:44 -0500

    semantic ui react

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Button, Icon } from "semantic-ui-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -6,6 +7,9 @@ export default function Home() {
     return (
         <div className="home">
             <h1>Estamos en next</h1>
+            <Button primary>Primary</Button>
+            <Button secondary>Secondary</Button>
+            <Icon disabled name="users" />
         </div>
     );
 }

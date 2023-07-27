aaea737af6aa22c28f95d2bb8283b43fedd84c2f
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-07-26 23:59:38 -0500

    layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import { Inter } from "next/font/google";
-import { Button, Icon } from "semantic-ui-react";
-
-const inter = Inter({ subsets: ["latin"] });
+import BasicLayout from "@/layouts/BasicLayout";
 
 export default function Home() {
     return (
-        <div className="home">
-            <h1>Estamos en next</h1>
-            <Button primary>Primary</Button>
-            <Button secondary>Secondary</Button>
-            <Icon disabled name="users" />
-        </div>
+        <BasicLayout>
+            <h1>Estamos en el home</h1>
+        </BasicLayout>
     );
 }
